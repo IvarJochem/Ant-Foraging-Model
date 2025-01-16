e2e0cfb73c1290cff8410c2e5b46a1714c1d9f89
lydiashumskaya <lydia.shumskaya@gmail.com>
2025-01-16 11:44:21 +0100

    Random seed toegevoegd aan maze generator

--- maze_generator_with_nPaths.py
+++ maze_generator_with_nPaths.py
@@ -38,7 +38,7 @@ def generate_maze_with_paths(rows, cols, path_count):
     maze = initialize_maze(rows, cols)
     start_x, start_y = 1, 1
     maze[start_x][start_y] = 0
-
+    random.seed(1275832698236592)
     # Track the number of paths created
     current_paths = [1]
     carve_passages_from(start_x, start_y, maze, rows, cols, path_count, current_paths)
