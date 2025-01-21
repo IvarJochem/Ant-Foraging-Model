b5af199785439f5fe0b6ba6b94b63ac31289c040
IvarJochem <Ivardj@xs4all.nl>
2025-01-21 18:12:04 +0100

    Multithreaded testgraph.py and headless_simulation.py

--- headless_simulation.py
+++ headless_simulation.py
@@ -3,72 +3,85 @@ import matplotlib.pyplot as plt
 from maze_generator_with_nPaths import generate_maze_with_paths, upscale_maze
 import ant_model_walkback as amw
 from ant_model_walkback import Model, Ant
+import os
+import multiprocessing as mp
 
+# Set the parameters for the simulation
+# Make sure to define the simulation parameters here or in run_process, as run_process won't have access to them otherwise
+amw.nPaths = 16
+amw.maze_dimention = 21
+amw.maze_scale = 3
+# release ants in waves
+amw.nAnts = 1000
+amw.nWaveAnts = 100
+amw.WaveTimesteps = 100
+
+amw.colony_position = (amw.maze_scale, amw.maze_scale)
+amw.food_position = (amw.maze_size-amw.maze_scale-1, amw.maze_size-amw.maze_scale-1)
+amw.ants_with_food_returned = 25
+amw.pheromone_deposit = 0.1
+amw.decay_rate = 0.9
+amw.max_pheromone = 0.99
+# number of food return
+amw.ants_with_food_returned = 200
+
+def run(tempFileName='simulation_results', folder_name="sim_results", initialRandomseed=16436, nStop=2500, iterations=5):
+    
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        print(f"Folder '{folder_name}' created.")
+    else:
+        print(f"Folder '{folder_name}' already exists.")
+
+    processes = []
+    for i in range(iterations):
+        p = mp.Process(target=run_process, args=(tempFileName, folder_name, initialRandomseed, nStop, i))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    print("All simulations finished.")
+
+def run_process(tempFileName, folder_name, initialRandomseed, nStop, i):
+    timeSteps = nStop
+    t = 0
+    #Je kan de multiplicatie met randomseed weghalen als je steeds dezelfde seed wil checken.
+    #Maze = generate_maze_with_paths(mazeCols, mazeRows, mazePaths, randomseed=initialRandomseed+i*152)
+    Maze = generate_maze_with_paths(amw.maze_dimention, amw.maze_dimention, amw.nPaths, randomseed=initialRandomseed)
+    Maze = upscale_maze(Maze, amw.maze_scale)
+    sim = Model(Maze, len(Maze[0]), len(Maze))
+    # Heb hier schrijven we de initial settings
+    filename = os.path.join(folder_name, f"{tempFileName}{i + 1}.txt")
+    with open(filename, "w") as file:
+        file.write(f"Simulation Iteration {i + 1}\n")
+        file.write(f"Maze Dimensions: {amw.maze_dimention}x{amw.maze_dimention}, Scale Factor: {amw.maze_scale}, Paths: {amw.nPaths}\n")
+        file.write(f"Number of Ants: {amw.nAnts}, Maximum Time Steps: {nStop}, Food Find Target: {amw.ants_with_food_returned}\n")
+        file.write(f"Initial Random Seed: {initialRandomseed}\n")
+        file.write(f"Pheromone decay rate: {amw.decay_rate}, Deposit rate: {amw.pheromone_deposit}, Max: {amw.max_pheromone}")
+        # this is the file for our baseline
+        if amw.pheromone_deposit==0:
+            file.write("Baseline: True\n")
+        else:
+            file.write("Baseline: False")
+        file.write("Starting simulation\n\n")
+        file.write("Timestep, Food Found, AntsFood\n")
+        print('Starting simulation')
+        n = amw.ants_with_food_returned
+        while t < timeSteps and sim.food_found < n:
+            amw.food_found = sim.update(t)  # Update simulation
+            # Je kan dingen van de model writen met sim. zoals bij sim.food_found
+            # en dingen van de mieren via het callen van sim.ants, hierna kan je met ant.hasfood bijvoorbeeld
+            # een lijst maken van elk geval waneer een mier eten draagt. Laatste parameter houdt dus bij hoeveel
+            # mieren eten dragen.
+            file.write(f"{t}, {sim.food_found}, {len([ant.hasfood for ant in sim.ants if ant.hasfood])}\n")
+
+            t += 1
 
 if __name__ == '__main__':
     """
     Simulation parameters
     """
-    amw.nPaths = 16
-    amw.maze_dimention = 21
-    amw.maze_scale = 3
-    # release ants in waves
-    amw.nAnts = 1000
-    amw.nWaveAnts = 100
-    amw.WaveTimesteps = 100
-
-    amw.colony_position = (amw.maze_scale, amw.maze_scale)
-    amw.food_position = (amw.maze_size-amw.maze_scale-1, amw.maze_size-amw.maze_scale-1)
-    amw.ants_with_food_returned = 25
-    amw.pheromone_deposit = 0.1
-    amw.decay_rate = 0.9
-    amw.max_pheromone = 0.99
-    # number of food return
-    amw.ants_with_food_returned = 200
-
-    def run(tempFileName='simulation_results', initialRandomseed=16436, nStop=2500, iterations=5):
-        import os
-        folder_name = "sim_results"
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-            print(f"Folder '{folder_name}' created.")
-        else:
-            print(f"Folder '{folder_name}' already exists.")
-
-
-        for i in range(iterations):
-            timeSteps = nStop
-            t = 0
-            #Je kan de multiplicatie met randomseed weghalen als je steeds dezelfde seed wil checken.
-            #Maze = generate_maze_with_paths(mazeCols, mazeRows, mazePaths, randomseed=initialRandomseed+i*152)
-            Maze = generate_maze_with_paths(amw.maze_dimention, amw.maze_dimention, amw.nPaths, randomseed=initialRandomseed)
-            Maze = upscale_maze(Maze, amw.maze_scale)
-            sim = Model(Maze, len(Maze[0]), len(Maze))
-            # Heb hier schrijven we de initial settings
-            filename = os.path.join(folder_name, f"{tempFileName}{i + 1}.txt")
-            with open(filename, "w") as file:
-                file.write(f"Simulation Iteration {i + 1}\n")
-                file.write(f"Maze Dimensions: {amw.maze_dimention}x{amw.maze_dimention}, Scale Factor: {amw.maze_scale}, Paths: {amw.nPaths}\n")
-                file.write(f"Number of Ants: {amw.nAnts}, Maximum Time Steps: {nStop}, Food Find Target: {amw.ants_with_food_returned}\n")
-                file.write(f"Initial Random Seed: {initialRandomseed}\n")
-                file.write(f"Pheromone decay rate: {amw.decay_rate}, Deposit rate: {amw.pheromone_deposit}, Max: {amw.max_pheromone}")
-                # this is the file for our baseline
-                if amw.pheromone_deposit==0:
-                    file.write("Baseline: True\n")
-                else:
-                    file.write("Baseline: False")
-                file.write("Starting simulation\n\n")
-                file.write("Timestep, Food Found, AntsFood\n")
-                print('Starting simulation')
-                n = amw.ants_with_food_returned
-                while t < timeSteps and sim.food_found < n:
-                    amw.food_found = sim.update(t)  # Update simulation
-                    # Je kan dingen van de model writen met sim. zoals bij sim.food_found
-                    # en dingen van de mieren via het callen van sim.ants, hierna kan je met ant.hasfood bijvoorbeeld
-                    # een lijst maken van elk geval waneer een mier eten draagt. Laatste parameter houdt dus bij hoeveel
-                    # mieren eten dragen.
-                    file.write(f"{t}, {sim.food_found}, {len([ant.hasfood for ant in sim.ants if ant.hasfood])}\n")
-
-                    t += 1
 
     run()
--- testgraph.py
+++ testgraph.py
@@ -1,336 +1,66 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from maze_generator_with_nPaths import generate_maze_with_paths, upscale_maze
+import ant_model_walkback as amw
+from ant_model_walkback import Model, Ant
 import random
+import multiprocessing as mp
+import time
 
-# Number of paths change this to 120, 55, 32, or 16
-nPaths = 16
+# Set the parameters for the simulation
+# Make sure to define the simulation parameters here or in run_process, as run_process won't have access to them otherwise
+# amw.ntimeSteps = 2500
+# amw.maze_dimention = 31
+# amw.maze_scale = 2
 
-# Number of time steps in the simulation 
-ntimeSteps = 1000
+def run():
+    start_time = time.time()
 
-# Maze dimention, must be odd number 
-maze_dimention = 13
+    #List with times for each dfficulty
+    results = []
 
-# Maze scale (the width of the paths)
-maze_scale = 3
+    #Number of iterations
+    iteration = 10
 
-# Maze size
-maze_size = maze_dimention*maze_scale
+    # 32 bit max value
+    MAX_INT = 32**2 - 1
 
-# Colony position
-colony_position = (maze_scale, maze_scale)
-
-# Food position
-food_position = (maze_size-maze_scale-1, maze_size-maze_scale-1)
-
-# Number of ants
-nAnts = 100
-
-# Number of ants per wave
-nWaveAnts = 5
-
-# Time step between waves
-WaveTimesteps = 10
-
-# Number of ants that have to return with food 
-ants_with_food_returned = 50
-
-# Amount of pheromone deposited per timestep per ant
-pheromone_deposit = 0.1
-
-# Decay rate of pheromones
-decay_rate = 0.01
-
-# Maximum amount of pheromones per cell
-max_pheromone = 0.99
-
-# Cell types
-colony = -1
-open_space = 0
-food = 1
-wall = 2
-
-
-class Model:
-    def __init__(self, maze, width, height):
-        """
-        Initialize the model with width, height, and number of ants.
-        """
-        self.width = width
-        self.height = height
-        self.nAnts = nAnts
-        self.nWaveAnts = nWaveAnts
-        self.WaveTimesteps = WaveTimesteps
-        self.total_ants_spawned = 0
-
-        # Our grid is a maze with walls (2) and open spaces (0)
-        self.grid = maze
-
-        # We initialize colony in the top left and the food at the bottom right
-        self.colony_position = colony_position
-        self.food_position = food_position
-
-        # Colony is represented as a -1 and food as a 1
-        self.grid[self.colony_position] = colony
-        self.grid[self.food_position] = food
-        # Initialize pheromone grid
-        self.pheromones = np.zeros_like(maze, dtype = float)
-        # Initialize ants at the colony
-        self.ants = []
-
-        # Track the food deliverd
-        self.food_found = 0
-
-        # Track if food is discovered
-        self.food_discovered = False
-
-    def spawn_ants(self):
-        """
-        Spawn a wave of ants if the maximum number of ants has not been reached.
-        """
-        if self.total_ants_spawned < self.nAnts:
-            new_ants = min(self.nWaveAnts, self.nAnts - self.total_ants_spawned)
-            for _ in range(new_ants):
-                self.ants.append(Ant(self.grid, self.pheromones, self.colony_position))
-            self.total_ants_spawned += new_ants
-
-    def update(self, timestep):
-        """
-        Update the positions of all ants and stop if food is found.
-        """
-        if timestep % self.WaveTimesteps == 0:
-            self.spawn_ants()
-
-        self.pheromones *= (1-decay_rate)
-        for ant in self.ants:
-            ant.step(0.1)  # Update position and direction
-            # Check if an ant has found the food
-            if ant.hasfood and (ant.position[0], ant.position[1]) == self.colony_position:
-                self.food_discovered = True
-                self.food_found += 1  # Increment the food delivered count
-                ant.hasfood = False
-                ant.visited = {}
-                ant.visited[str(ant.position)] = True
-                ant.path = []
-                ant.path.append(ant.position)
-                ant.time_since_last_update = 0.0
-
-
-class Ant:
-    def __init__(self, maze, pheromones, colony_position):
-        """
-        Class to model the ants. Each ant is initialized with a position and direction.
-        """
-        self.position = colony_position
-        self.colony_position = colony_position
-        self.has_moved_away = False
-        self.grid = maze
-        self.visited = {}
-        self.visited[str(self.position)] = True
-        self.path = []
-        self.path.append(self.position)
-        self.time_since_last_update = 0.0
-        self.hasfood = False
-        self.pheromones = pheromones
-
-    def get_adjacent_cells(self):
-        """
-        Get the adjacent cells of the ant in the grid.
-        """
-        x, y = self.position
-        adj_cells = [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]
-        return adj_cells
-
-    def choose_cells_based_on_pheromones(self, adj_cells):
-        """
-        Choose one of adjecent cells based on pheromone level
-        """
-        # remove cells that are walls
-        adj_cells = [cell for cell in adj_cells if self.grid[cell[0], cell[1]] != wall]
-        # remove cells that are visited
-        adj_cells = [cell for cell in adj_cells if str(cell) not in self.visited.keys()]
-
-        if len(adj_cells) == 0:
-            return False
-
-        total_chance = 0
-        for cell in adj_cells:
-            total_chance += self.pheromones[cell[0], cell[1]] + 0.1
-
-        if total_chance == len(adj_cells)*0.1:
-            # nothing happens, no pheromones
-            np.random.shuffle(adj_cells)
-            return adj_cells[0]
-        # pick a random number between 0 and the total pheromones
-        pheromone_pick  = np.random.uniform(0, total_chance)
-        current_chance = 0
-        # pick the cell based on pheromone_pick
-        for cell in adj_cells:
-            current_chance += self.pheromones[cell[0], cell[1]] + 0.1
-            if current_chance >= pheromone_pick:
-                return cell
-
-        return False
-
-    def step(self, dt):
-        """Update the ant's state for the given time step."""
-        self.time_since_last_update += dt
-        x, y = self.position
-        current_cell_value = self.grid[x, y]
-        # check if the ant found food
-        if current_cell_value == food:
-            self.hasfood = True
-
-        if self.hasfood:
-            if self.position == self.colony_position:
-                self.hasfood = False
-                self.visited = {}
-                self.visited[str(self.position)] = True
-                self.path = []
-                self.path.append(self.position)
-                return
-            else:
-                if current_cell_value >= 0 and current_cell_value < 1:
-                    self.pheromones[x, y] = min(self.pheromones[x, y] +  pheromone_deposit , max_pheromone)
-        else:
-            adj_cells = self.get_adjacent_cells()
-            # choose the next cell based on pheromones
-            cell = self.choose_cells_based_on_pheromones(adj_cells)
-            if cell != False:
-                self.position = cell
-                self.visited[str(self.position)] = True
-                self.path.append(self.position)
-                self.time_since_last_update = 0.0
-                return
-        # move back to the colony along the path
-        self.path.pop()
-        if len(self.path) > 0:
-            self.position = self.path[-1]
-        self.time_since_last_update = 0.0
-        return
-
-
-class Visualization:
-    def __init__(self, maze, pheromones, height, width, pauseTime=0.01):
-        """
-        This simple visualization shows the colony, food, and ants.
-        """
-        self.h = height
-        self.w = width
-        self.pauseTime = pauseTime
-        self.grid = maze.copy()
-        self.pheromones = pheromones
-        from matplotlib.colors import ListedColormap
-        colors = [
-            "purple", # -3 ants with food
-            "red", # -2 ants no food
-            "yellow", #-1 colony
-            "white", # 0 open spaces
-            "green", # 1 food
-            "black", # 2 walls
-
-        ]
-        custom_cmap = ListedColormap(colors)
-
-        self.im = plt.imshow(self.grid, vmin=-3, vmax=3, cmap=custom_cmap)
-
-        self.ph_im = None
-        plt.title('Ant Simulation')
-
-    def update(self, t, ant_without_food_positions, ant_with_food_positions):
-        """
-        Updates the grid with colony, food, and ants for visualization.
-        """
-        self.grid = maze.copy()
-
-        # Visualize the pheromones
-        if self.ph_im is None:
-            self.ph_im = plt.imshow(self.pheromones, alpha=0.5, cmap='hot', vmin=0, vmax=max_pheromone)
-        else:
-            self.ph_im.set_data(self.pheromones)
-        # # Visualize the colony with a 3x3 radius in yellow (-1)
-        # for dx in [-1, 0, 1]:
-        #     for dy in [-1, 0, 1]:
-        #         x = (colony_position[0] + dx) % self.h
-        #         y = (colony_position[1] + dy) % self.w
-        #         grid[x, y] = -1
-
-        # # Visualize the food (2)
-        # grid[food_position[0], food_position[1]] = 2
-
-        # Visualize the ants (-2)
-        for x, y in ant_without_food_positions:
-            self.grid[x, y] = -2
-
-        # Visualize the ants with food (-3)
-        for x, y in ant_with_food_positions:
-            self.grid[x, y] = -3
-
-        self.im.set_data(self.grid)
-
-        plt.draw()
-        plt.title('t = %i' % t)
-        plt.pause(self.pauseTime)
-
-    def persist(self):
-        """
-        Use this method if you want to have the visualization persist after the
-        calling the update method for the last time.
-        """
-        plt.show()
-
-
-if __name__ == '__main__':
-     """
-    Simulation parameters
-    """
-     timeSteps = ntimeSteps
-     #List with times for each dfficulty 
-     results = []  
-
-     #Number of iterations
-     iteration = 100
-
-     
     # Different maze diffuctlies
-     for nPaths in [16, 32, 55, 120]: 
-        times = []
+    for nPaths in [16, 32, 55, 120]:
+        print(f'Starting simulation with {nPaths} paths')
+        # Shared array to store the times for each iteration
+        shared_times = mp.Array('i', [-1] * iteration)
+        # List to store the processes
+        processes = []
         for i in range(iteration):
-            random.seed(i)
-            maze = generate_maze_with_paths(maze_dimention, maze_dimention, nPaths)
-            maze = upscale_maze(maze, maze_scale)
-            sim = Model(maze, len(maze[0]), len(maze))
-            #vis = Visualization(maze, sim.pheromones, sim.height, sim.width)
-            
-            #Time of current iteration
-            t = 0
-            print(f'Starting simulation with {nPaths} paths')
+            # Create a new process for each iteration
+            p = mp.Process(target=run_process, args=(MAX_INT, nPaths, shared_times, i))
+            processes.append(p)
+            p.start()
 
-            # Run the simulation for the current value of nPaths
-            while t < timeSteps:
-                sim.update(t)  # Update simulation
-                ant_without_food_positions = [ant.position for ant in sim.ants if not ant.hasfood]
-                ant_with_food_positions = [ant.position for ant in sim.ants if ant.hasfood]
-                #vis.update(t, ant_without_food_positions, ant_with_food_positions)
+        # Wait for all processes to finish
+        for p in processes:
+            p.join()
 
-                # Check if enough ants have returned with food
-                if sim.food_found > ants_with_food_returned - 1:
-                    times.append(t)  
-                    break
+        # Count the number of simulations that ran out of time
+        failed_count = 0
+        for t in enumerate(shared_times):
+            if t[1] == -1:
+                failed_count += 1
+                shared_times[t[0]] = amw.ntimeSteps
 
-                t += 1
+        print(f'Timed out simulations: {failed_count}')
 
-        avg_time = sum(times) / len(times)
-        results.append((nPaths, avg_time)) 
+        avg_time = sum(shared_times) / len(shared_times)
+        results.append((nPaths, avg_time))
 
         #vis.persist()  # Keep the visualization open after the simulation
 
     # Plotting the results after all simulations
-     if results:
+    if results:
         print(results)
-
+        # print the time it took to run the simulation
+        print(f"--- {time.time() - start_time} seconds ---")
         # Extract x (number of paths) and y (time to find food) values
         x_values, y_values = zip(*results)
 
@@ -341,3 +71,33 @@ if __name__ == '__main__':
         plt.ylabel('Foraging time')
         plt.title('Effect of maze difficulty on foraging time')
         plt.show()
+
+def run_process(MAX_INT, nPaths, times, i):
+    new_seed = random.randint(0, MAX_INT)
+    maze = generate_maze_with_paths(amw.maze_dimention, amw.maze_dimention, nPaths, randomseed=new_seed)
+    maze = upscale_maze(maze, amw.maze_scale)
+    # from ant_model_walkback import Model
+    sim = Model(maze, len(maze[0]), len(maze))
+
+    #Time of current iteration
+    t = 0
+
+    # Run the simulation for the current value of nPaths
+    while t < amw.ntimeSteps:
+        sim.update(t)  # Update simulation
+
+        # Check if enough ants have returned with food
+        if sim.food_found > amw.ants_with_food_returned - 1:
+            times[i] = t
+            return
+
+        t += 1
+
+
+if __name__ == '__main__':
+    """
+    Simulation parameters
+    """
+    run()
+
+
